82e691cf847b820666c906a826deff4b8be82aab
Srinidhi Murthy <esmirur@seliiuvd03786.seli.gic.ericsson.se>
2020-07-05 23:58:52 +0100

    Adding Python crash couurse folder and files

---
+++ Python_crash_course/example1.py
@@ -0,0 +1,5 @@
+welcome = "Hey welcome to the world of python"
+print(welcome)
+
+welcome = "Hey there,this is python"
+print(welcome)
---
+++ Python_crash_course/fstrings.py
@@ -0,0 +1,5 @@
+first_name = "John"
+last_name = "Man"
+full_name = f"{first_name} {last_name}"
+
+print(f"Hello, {full_name.title()}")
