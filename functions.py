70655687d396f794950d821cb32b513baf9acfad
Srinidhi Murthy <esmirur@seliiuvd03786.seli.gic.ericsson.se>
2020-11-19 13:23:23 +0000

    more functions

--- functions.py
+++ functions.py
@@ -10,3 +10,23 @@ def func2(a, b):
 
 subtract = func2(20, 8)
 print(subtract)
+
+def bmi_calc(name, wt_kg, ht_m):
+    bmi = wt_kg / (ht_m **2)
+    
+    print("bmi is ")
+    print(bmi)
+    
+
+    if bmi > 25:
+       return name + " is overweight"
+    else:
+        return name + " is not overweight"
+
+namea = "Sri"
+wt_kga = 72
+ht_ma = 1.70
+
+
+Person1 = bmi_calc(namea, wt_kga, ht_ma)
+print (Person1)
