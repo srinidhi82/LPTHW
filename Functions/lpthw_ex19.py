3c0c1acdff490d2d7da25d9521e71b39549317af
Srinidhi <srinidhi23@gmail.com>
2020-11-25 00:28:28 +0000

    functions add

--- Functions/lpthw_ex19.py
+++ Functions/lpthw_ex19.py
@@ -2,13 +2,9 @@ def cheese_and_crisp(cheese_amount, crisp_packets):
     print(f"You have {cheese_amount} and {crisp_packets}")
 
 
-def cheese_brand_and_crisp_name("cheese_brand", "crisp_name"):
-    print(f"You have {cheese_brand} and {crisp_name}")
-
-
 print("Pass any 2 arguments")
 cheese_and_crisp(20, 50)
-cheese_brand_and_crisp_name("Cottage cheese", "Tayto chips")
+cheese_and_crisp("Cottage cheese", "Tayto chips")
 
 print("Use variables and its value as input to function")
 cheese = 20
